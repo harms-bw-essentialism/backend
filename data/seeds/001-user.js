7d0736b4e4a9870557692b80a6b7ae6bbc40eb07
Aaron Williams <aaronrw4@yahoo.com>
2020-02-27 20:22:09 -0800

    Updated database with tokens and hash

--- data/seeds/001-user.js
+++ data/seeds/001-user.js
@@ -1,9 +1,13 @@
+const bcrypt = require('bcryptjs');
+const password = 'pass'
+const hash = bcrypt.hashSync(password, 10);
 
 exports.seed = function(knex) {
-  return knex('users').del()
+  return knex('users')
+    .truncate()
     .then(function () {
       return knex('users').insert([
-        {username: 'Michael', password: 'pass'}
+        {username: 'Michael', password: hash}
       ]);
     });
 };
