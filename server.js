7cbfbdae3fef4b62ec1f36f19c514ad4398abf1d
Aaron Williams <aaronrw4@yahoo.com>
2020-02-28 20:05:45 -0800

    Added projects route

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const ValuesRouter = require('./essentialism/valuesRouter');
-const AuthRouter = require('./essentialism-auth/authRouter')
+const ProjectsRouter = require('./essentialism/projectsRouter');
+const AuthRouter = require('./essentialism-auth/authRouter');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/essentialism/values', ValuesRouter);
+server.use('/api/essentialism/projects', ProjectsRouter);
 server.use('/api/essentialism/user', AuthRouter);
 
 module.exports = server;
