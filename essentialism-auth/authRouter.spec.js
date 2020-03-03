faa4ff7115bd422cb2e0242e57b5f499e5c5f0ed
Aaron Williams <aaronrw4@yahoo.com>
2020-03-02 19:37:05 -0800

    Created tests for authRouter

---
+++ essentialism-auth/authRouter.spec.js
@@ -0,0 +1,76 @@
+const request = require('supertest');
+const server = require('../server');
+const db = require('../data/dbConfig');
+
+describe('Auth routes', () => {
+    describe('Post /register', () => {
+        it('Should return 201', async (done) => {
+            await db('users').truncate()
+
+            request(server)
+                .post('/api/essentialism/user/register')
+                .send({username: 'Aaron', password: 'pass'})
+                .expect(201, done);
+        })
+        it('Should return json', () => {
+            request(server)
+                .post('/api/essentialism/user/register')
+                .send({username: 'Aaron', password: 'pass'})
+                .expect('Content-Type', /json/)
+        })
+    });
+
+    describe('Post /login', () => {
+        it('Should return 200', async (done) => {
+            request(server)
+                .post('/api/essentialism/user/login')
+                .send({username: 'Aaron', password: 'pass'})
+                .expect(200, done)
+        })
+        it('Should return json', () => {
+            request(server)
+                .post('/api/essentialism/user/login')
+                .send({username: 'Aaron', password: 'pass'})
+                .expect('Content-Type', /json/)
+        })
+    })
+
+    let token;
+
+    beforeAll((done) => {
+        request(server)
+            .post('/api/essentialism/user/login')
+            .send({username: 'Aaron', password: 'pass'})
+            .end((err, res) => {
+                token = res.body.token;
+                done();
+            })
+    })
+
+    describe('Get /', () => {
+        it('Should return 401 with no auth', (done) => {
+            request(server)
+                .get('/api/essentialism/user')
+                .expect(401, done)
+        })
+        it('Should return 200 with auth', (done) => {
+            request(server)
+                .get('/api/essentialism/user')
+                // .set('authorization', token)
+                .expect(200, done)
+        })
+    })
+
+    describe('Delete /:id', () => {
+        it('Should return 200', (done) => {
+            request(server)
+                .delete('/api/essentialism/user/1')
+                .expect(200, done)
+        })
+        it('Return an empty db.users', async () => {
+            const usersDB = await db('users');
+            expect(usersDB).toHaveLength(0)
+                
+        })
+    })
+})
